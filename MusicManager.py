2c486cd3ea8c547051b65cff5c3ef76c5b37f490
shiftking <dylan.t.rush0@gmail.com>
2017-02-23 23:09:48 -0600

    Added main program file and updated readme

---
+++ MusicManager.py
@@ -0,0 +1,3 @@
+#Main management Script
+#This will scrape that data from a users account, peramiters can be set for downloading certains songs or entire playlists
+#metadata is preserved as well as playlist or albumn structure
